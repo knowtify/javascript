ab86c0bef06bad68d98d34c7dd973fd6e4fc2aed
Dane Lyons <duilen@gmail.com>
2014-10-01 15:24:35 -0700

    css file

--- contacts_form.js
+++ contacts_form.js
@@ -1,6 +1,14 @@
 (function(){
 if(window.knowtifyJS){
-	var k = window.knowtifyJS;
-	console.log(k);
+	//Not happy about making jquery a dependancy but not going native for v1...
+	if (typeof jQuery == 'undefined') {
+	    var script = document.createElement('script');
+	    script.type = "text/javascript";
+	    script.src = "http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js";
+	    document.getElementsByTagName('head')[0].appendChild(script);
+	}
+	$('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', 'http://js.knowtify.io.s3-website-us-east-1.amazonaws.com/contacts_form.css') );
+
+	$('body').append('<div id="knowtifyjs_button">Subscribe</div>');
 }
 })();
